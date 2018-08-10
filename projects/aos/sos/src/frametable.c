b17cd7d498e97622f744c714c7e8927cbc276d41
Tianle Qiu <z5174807@unsw.edu.au>
2018-08-10 18:00:33 +1000

    milestone 1

--- projects/aos/sos/src/frametable.c
+++ projects/aos/sos/src/frametable.c
@@ -2,13 +2,24 @@
 #include "ut.h"
 
 #define FRAME_BASE 0xA000000000
+#define UNTYPE_MEMEORY 0x1
+#define FREE_MEMORY 0x2
+#define USED_MEMORY 0x3
+#define MEMORY_TYPE_MASK 0x3
 
 typedef struct frame_table_obj {
     ut_t *ut;
+    int next;
     uint16_t flag;
 } frame_table_obj;
 
-static frame_table_obj *frame_table;
+typedef struct frame_table {
+    int free;
+    frame_table_obj *frames;
+} frame_table;
+
+static frame_table frame_table;
+static cspace_t *root_cspace;
 
 static ut_t *alloc_retype(seL4_CPtr *cptr, seL4_Word type)
 {
@@ -20,7 +31,7 @@ static ut_t *alloc_retype(seL4_CPtr *cptr, seL4_Word type)
     }
 
     /* allocate a slot to retype the memory for object into */
-    *cptr = cspace_alloc_slot(&cspace);
+    *cptr = cspace_alloc_slot(root_cspace);
     if (*cptr == seL4_CapNull) {
         ut_free(ut, seL4_PageBits);
         ZF_LOGE("Failed to allocate slot");
@@ -28,33 +39,79 @@ static ut_t *alloc_retype(seL4_CPtr *cptr, seL4_Word type)
     }
 
     /* now do the retype */
-    seL4_Error err = cspace_untyped_retype(&cspace, ut->cap, *cptr, type, seL4_PageBits);
+    seL4_Error err = cspace_untyped_retype(root_cspace, ut->cap, *cptr, type, seL4_PageBits);
     ZF_LOGE_IFERR(err, "Failed retype untyped");
     if (err != seL4_NoError) {
         ut_free(ut, size_bits);
-        cspace_free_slot(&cspace, *cptr);
+        cspace_free_slot(root_cspace, *cptr);
         return NULLseL4_PageBits;
     }
     return ut;
 }
 
-void initialize_frame_table(cspace_t *cspace, seL4_CPtr vspace, seL4_Word vaddr, seL4_CapRights_t rights,
-                     seL4_ARM_VMAttributes attr)
+void initialize_frame_table(cspace_t *cspace, seL4_CPtr vspace, seL4_Word vaddr)
 {
     ut_t *ut;
     seL4_Word vaddr = FRAME_BASE;
-    frame_table = (frame_table_obj *) FRAME_BASE;
+    frame_table.frames = (frame_table_obj *) FRAME_BASE;
+    // the number of pages of all untyped memeory
     size_t size = ut_size() / PAGE_SIZE_4K;
-    for(int i = 0; i < size; ++i) {
-        ut = alloc_retype(cspace, seL4_ARM_SmallPageObject, sel4_)
+    // the number of pages consumed by frame table
+    size_t n_pages = size * sizoef(frame_table_obj) / PAGE_SIZE_4K;
+    frame_table.free = (uint32_t) n_pages;
+    for(int i = 0; i < n_pages; ++i) {
+        seL4_CPtr frame_cap;
+        ut = alloc_retype(&frame_cap, seL4_ARM_SmallPageObject);
         if (ut == NULL) {
             return;
         }
-        ZF_LOGF_IF(frame == NULL, "Failed to allocate frame table page");
-        seL4_Error err = map_frame(&cspace, frame_cap, seL4_CapInitThreadVSpace,
+        ZF_LOGF_IF(ut == NULL, "Failed to allocate frame table page");
+        seL4_Error err = map_frame(cspace, frame_cap, seL4_CapInitThreadVSpace,
                                    vaddr, seL4_AllRights, seL4_ARM_Default_VMAttributes);
         ZF_LOGF_IFERR(err, "Failed to map frame table pages");
+        frame_table.frames[i].ut = ut;
+        frame_table.frames[i].next = -1;
+        frame_table.frames[i].flag = USED_MEMORY;
         vaddr += PAGE_SIZE_4K;
     }
+    for(int i = n_pages; i < size; ++i) {
+        frame_table.frames[i].ut = NULL;
+        frame_table.frames[i].next = i + 1;
+        frame_table.frames[i].flag = UNTYPE_MEMEORY;
+    }
+    frame_table.frames[size - 1].next = -1;
+    root_cspace = cspace;
+    return;
+}
+
+int frame_alloc(seL4_Word *vaddr)
+{
+    int page = frame_table.free;
+    seL4_CPtr frame_cap;
+    *vaddr = 0;
+    if(page == -1) {
+        return -1;
+    }
+    ut_t *ut = alloc_retype(&frame_cap, seL4_ARM_SmallPageObject);
+    if (ut == NULL) {
+        return -1;
+    }
+    *vaddr = page * PAGE_SIZE_4K + FRAME_BASE;
+    seL4_Error err = map_frame(root_cspace, frame_cap, seL4_CapInitThreadVSpace,
+                                *vaddr, seL4_AllRights, seL4_ARM_Default_VMAttributes);
+    /*
+    if(err != seL4_NoError) {
+        return -1;
+    }
+    */
+    frame_table.frames[page].ut = ut;
+    frame_table.frames[page].flag = USED_MEMORY;
+    frame_table.free = frame_table.frames[page].next;
+    return page;
+    
+}
+
+void frame_free(int frame)
+{
 
 }
--- projects/aos/sos/src/frametable.h
+++ projects/aos/sos/src/frametable.h
@@ -2,9 +2,10 @@
 
 #include <stdint.h>
 #include <sel4/sel4.h>
+#include<cspace/cspace.h>
 
-void initialize_frame_table(void);
+void initialize_frame_table(cspace_t *cspace, seL4_CPtr vspace, seL4_Word vaddr);
 
-uint32_t frame_alloc(seL4_Word *vaddr);
+int frame_alloc(seL4_Word *vaddr);
 
-void frame_free(uint32_t frame);
+void frame_free(int frame);

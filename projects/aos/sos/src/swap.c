d9a72a76cf41da74ed39f028cf731c1373c15449
Tianle Qiu <z5174807@unsw.edu.au>
2018-10-14 14:28:42 +1100

    minor changes

--- projects/aos/sos/src/swap.c
+++ projects/aos/sos/src/swap.c
@@ -168,10 +168,15 @@ seL4_Error try_swap_out(void)
 
 void clean_up_swapping(unsigned offset)
 {
+    while (swap_lock == 1) {
+        yield(NULL);
+    }
+    swap_lock = 1;
     struct uio k_uio;
     int result;
     uio_kinit(&k_uio, (seL4_Word)&header, sizeof(unsigned), offset, UIO_WRITE);
     result = VOP_WRITE(swap_file, &k_uio);
     header = offset / PAGE_SIZE_4K;
     assert(result == 0);
+    swap_lock = 0;
 }
